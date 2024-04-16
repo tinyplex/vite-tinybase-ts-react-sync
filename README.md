b7b6b1a9c05bef21854b4bafc51e585e4f374b79
James Pearce <github@tripleodeon.com>
2024-04-16 10:29:04 -0600

    First app

---
+++ README.md
@@ -0,0 +1,56 @@
+# vite-tinybase-ts-react
+
+This is a [Vite](https://vitejs.dev/) template for a simple
+[TinyBase](https://tinybase.org/) app, using TypeScript and React, and also
+demonstrating the TinyBase ui-react-dom module UI components.
+
+## Instructions
+
+1. Make a copy of this template into a new directory:
+
+```sh
+npx degit tinyplex/vite-tinybase-ts-react my-tinybase-app
+```
+
+2. Go into the directory:
+
+```sh
+cd my-tinybase-app
+```
+
+3. Install the dependencies:
+
+```sh
+npm install
+```
+
+4. Run the application:
+
+```sh
+npm run dev
+```
+
+5. Go the URL shown and enjoy!
+
+![](https://tinybase.org/vite-tinybase-2.png)
+
+## Other templates
+
+There are seven templates for TinyBase, of which this is one:
+
+|     | Template                                                                                       | Language   | React | Plus        |
+| --- | ---------------------------------------------------------------------------------------------- | ---------- | ----- | ----------- |
+|     | [vite-tinybase](https://github.com/tinyplex/vite-tinybase)                                     | JavaScript | No    |             |
+|     | [vite-tinybase-ts](https://github.com/tinyplex/vite-tinybase-ts)                               | TypeScript | No    |             |
+|     | [vite-tinybase-react](https://github.com/tinyplex/vite-tinybase-react)                         | JavaScript | Yes   |             |
+| 👉  | [vite-tinybase-ts-react](https://github.com/tinyplex/vite-tinybase-ts-react)                   | TypeScript | Yes   |             |
+|     | [vite-tinybase-ts-react-crsqlite](https://github.com/tinyplex/vite-tinybase-ts-react-crsqlite) | TypeScript | Yes   | CR-SQLite   |
+|     | [tinybase-ts-react-partykit](https://github.com/tinyplex/tinybase-ts-react-partykit)           | TypeScript | Yes   | PartyKit    |
+|     | [tinybase-ts-react-electricsql](https://github.com/tinyplex/tinybase-ts-react-electricsql)     | TypeScript | Yes   | ElectricSQL |
+
+## License
+
+This template has no license, and so you can use it however you want!
+[TinyBase](https://github.com/tinyplex/tinybase/blob/main/LICENSE) and
+[Vite](https://github.com/vitejs/vite/blob/main/LICENSE) themselves are both MIT
+licensed.
---
+++ src/App.tsx
@@ -0,0 +1,94 @@
+import { StrictMode } from 'react';
+import {
+  createMergeableStore,
+  MergeableStore,
+  Synchronizer,
+} from 'tinybase/debug';
+import { createSessionPersister } from 'tinybase/debug/persisters/persister-browser';
+import { createWsSynchronizer } from 'tinybase/debug/synchronizers/synchronizer-ws-client';
+import {
+  Provider,
+  useCreateMergeableStore,
+  useCreatePersister,
+  useCreateSynchronizer,
+} from 'tinybase/debug/ui-react';
+import {
+  SortedTableInHtmlTable,
+  StoreInspector,
+  ValuesInHtmlTable,
+} from 'tinybase/debug/ui-react-dom';
+import { Buttons } from './Buttons';
+
+export const App = () => {
+  const store = useCreateMergeableStore(() =>
+    createMergeableStore('' + Math.random())
+  );
+
+  useCreatePersister(
+    store,
+    (store) => createSessionPersister(store, 's'),
+    [],
+    async (persister) => {
+      await persister.startAutoLoad([
+        {
+          pets: { '0': { name: 'fido', species: 'dog' } },
+          species: {
+            dog: { price: 5 },
+            cat: { price: 4 },
+            fish: { price: 2 },
+            worm: { price: 1 },
+            parrot: { price: 3 },
+          },
+        },
+        { counter: 0 },
+      ]);
+      await persister.startAutoSave();
+    }
+  );
+
+  useCreateSynchronizer(
+    store,
+    async (store: MergeableStore) =>
+      await createWsSynchronizer(
+        store,
+        new WebSocket('ws://api-tinybase-org.fly.dev' + location.pathname),
+        1
+      ),
+    [],
+    async (synchronizer: Synchronizer) => {
+      await synchronizer.startSync();
+    }
+  );
+
+  return (
+    <StrictMode>
+      <Provider store={store}>
+        <Buttons />
+        <div>
+          <h2>Values</h2>
+          <ValuesInHtmlTable />
+        </div>
+        <div>
+          <h2>Species Table</h2>
+          <SortedTableInHtmlTable
+            tableId='species'
+            cellId='price'
+            descending={true}
+            sortOnClick={true}
+            className='sortedTable'
+          />
+          <h2>Pets Table</h2>
+          <SortedTableInHtmlTable
+            tableId='pets'
+            cellId='name'
+            limit={5}
+            sortOnClick={true}
+            className='sortedTable'
+            paginator={true}
+          />
+        </div>
+        <StoreInspector />
+      </Provider>
+    </StrictMode>
+  );
+};
